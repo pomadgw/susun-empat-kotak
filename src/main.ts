ae41969798620d60ab011ec693296e1a8b1f82b1
Rahadian Yusuf <rahadian.y@gmail.com>
2021-05-31 20:15:38 +0700

    create matrix and its rotation function

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 // import { createApp } from 'vue'
 // import App from './App.vue'
 
-import wasm, { greet } from '../wasm-src/pkg'
+import wasm, { Matrix } from '../wasm-src/pkg'
 
 // createApp(App).mount('#app')
 
 wasm().then((e) => {
-  greet()
+  const m = Matrix.new(3, 3)
+  m.set(0, 0, 1)
+  m.set(0, 1, 2)
+  m.set(0, 2, 3)
+  console.log(m.to_uint8array)
+  m.rotate_right()
+  console.log(m.to_uint8array)
 })
--- wasm-src/src/lib.rs
+++ wasm-src/src/lib.rs
@@ -1,12 +1,77 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use js_sys::*;
+
+use std::ops::{Index, IndexMut};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct Matrix {
+    pub width: usize,
+    pub height: usize,
+    matrix: Vec<u8>,
+}
+
+type Coord = (usize, usize);
+
+#[wasm_bindgen]
+impl Matrix {
+    pub fn new(width: usize, height: usize) -> Self {
+        Matrix {
+            width: width,
+            height: height,
+            matrix: vec![0; width * height],
+        }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn to_uint8array(&self) -> Uint8Array {
+        return Uint8Array::from(&self.matrix[..]);
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> u8 {
+        self[(x, y)]
+    }
+
+    pub fn set(&mut self, x: usize, y: usize, value: u8) {
+        self[(x, y)] = value;
+    }
+
+    pub fn rotate_right(&mut self) {
+        assert_eq!(self.width, self.height);
+        let n = self.height;
+
+        for i in 0..(n / 2) {
+            for j in i..(n - i - 1) {
+                let temp = self[(i, j)];
+                self[(i, j)] = self[(n - 1 - j, i)];
+                self[(n - 1 - j, i)] = self[(n - 1 - i, n - 1 - j)];
+                self[(n - 1 - i, n - 1 - j)] = self[(j, n - 1 - i)];
+                self[(j, n - 1 - i)] = temp;
+            }
+        }
+    }
+}
+
+impl Index<Coord> for Matrix {
+    type Output = u8;
+
+    fn index(&self, index: Coord) -> &Self::Output {
+        return &self.matrix[index.0 * self.height + index.1];
+    }
+}
+
+impl IndexMut<Coord> for Matrix {
+    fn index_mut(&mut self, index: Coord) -> &mut Self::Output {
+        &mut self.matrix[index.0 * self.height + index.1]
+    }
+}
 
 #[wasm_bindgen]
 extern {
